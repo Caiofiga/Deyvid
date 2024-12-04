deef22c601533c8a1a63bf70e7dafd2510744fa0
Caio Figueiredo <contascontao@gmail.com>
2024-12-04 14:58:26 -0300

    Oh noes john woes

--- py.py
+++ py.py
@@ -1,16 +1,32 @@
 import numpy as np
 
-ps = [5.3426, 1.7, 2.297]
+ps = [1, 30, 1]
 kqs = []
+alfa = [0.8, 1.95, 1.1]
+
+xs = []
+fxs = []
+
 
 def f(x):
-    numerator = (1.99 - x * (1 - x) * (2.15 - 3.93 * x + 2.7 * x**2))
-    denominator = ((1 + 2 * x) * (1 - x)**1.5)
+    if x < 0:
+        raise ValueError(
+            "x must be in the range [0, 1] to avoid imaginary results.")
+
+    numerator = 1.99 - x * (1 - x) * (2.15 - 3.93 * x + 2.7 * x**2)
+    denominator = (1 + 2 * x) * (1 - x)**1.5
+
     return 6 * np.sqrt(x) * (numerator / denominator)
 
-for i in range(3):
 
+for i in range(3):
+    x = alfa[i] / 13.4
     fx = f(x)
-    kq = ps[i]/(0.0044*np.sqrt(0.0134)) * fx
-    kqs.append()
-print(kqs)
+    kq = ps[i] / (0.0044 * np.sqrt(0.0134)) * fx
+    kqs.append((kq))
+    xs.append((x))
+    fxs.append((fx))
+
+print("xs:", xs)
+print("fxs:", fxs)
+print("kqs:", kqs)
